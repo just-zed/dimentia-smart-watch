06c795a1af7bb971553568edde5016862820a5c8
Freeman Man <freeman@ycfreeman.com>
2015-08-16 16:47:17 +1000

    create a common namespace for common constants

---
+++ DementiaSmartWatch/common/src/main/java/com/justzed/common/ApiKeys.java
@@ -0,0 +1,8 @@
+package com.justzed.common;
+
+public class ApiKeys {
+    public static final String PARSE_API_TEST_APPLICATION_ID = "9jYSMWurxNnfNHUb29UWDUrjCAJadpmCRNen9YKA";
+    public static final String PARSE_API_TEST_CLIENT_KEY = "LpYp4LO7RyjvBf9C32rSQiXYHfriTcTlQYCdpF45";
+    public static final String PARSE_API_PROD_APPLICATION_ID = "UAPzdvALR0oYGt1nJRciD4pvLIhpqAxaWFyBtaom";
+    public static final String PARSE_API_PROD_CLIENT_KEY = "uUFGLbFgoRJIoTs3rmrh0AB51PvXEy2jd31CyJjm";
+}
