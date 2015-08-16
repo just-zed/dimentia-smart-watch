9ae8778e2e04f0add88fc046aea421aba0ff6bda
Freeman Man <freeman@ycfreeman.com>
2015-08-16 22:04:31 +1000

    would need a defer for all these

--- DementiaSmartWatch/common/src/main/java/com/justzed/common/models/Person.java
+++ DementiaSmartWatch/common/src/main/java/com/justzed/common/models/Person.java
@@ -117,50 +117,52 @@ public class Person {
 
     public static Observable<Person> getByUniqueToken(String uniqueToken) {
 
-        return Observable.create(subscriber -> {
-            if (uniqueToken.isEmpty()) {
-                subscriber.onError(new Exception("unique token cannot be empty"));
-            }
-            ParseQuery<ParseObject> query = ParseQuery.getQuery(KEY_PERSON);
-            query.whereEqualTo(KEY_UNIQUE_TOKEN, uniqueToken);
-            query.setLimit(1);
-            query.findInBackground((list, e) -> {
-                if (e == null && list.size() == 1) {
-                    subscriber.onNext(deserialize(list.get(0)));
-                    subscriber.onCompleted();
-                } else if (list.size() == 0) {
-                    subscriber.onNext(null);
-                    subscriber.onCompleted();
-                } else {
-                    subscriber.onError(e);
-                }
-            });
-        });
+        return Observable.defer(() ->
+                Observable.create(subscriber -> {
+                    if (uniqueToken.isEmpty()) {
+                        subscriber.onError(new Exception("unique token cannot be empty"));
+                    }
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery(KEY_PERSON);
+                    query.whereEqualTo(KEY_UNIQUE_TOKEN, uniqueToken);
+                    query.setLimit(1);
+                    query.findInBackground((list, e) -> {
+                        if (e == null && list.size() == 1) {
+                            subscriber.onNext(deserialize(list.get(0)));
+                            subscriber.onCompleted();
+                        } else if (list.size() == 0) {
+                            subscriber.onNext(null);
+                            subscriber.onCompleted();
+                        } else {
+                            subscriber.onError(e);
+                        }
+                    });
+                }));
 
     }
 
 
     public Observable<Person> delete() {
-        return Observable.create(subscriber -> {
-            if (objectId == null) {
-                // this should never happen in the app
-                subscriber.onError(new Exception("incorrect usage"));
-            }
-            ParseQuery<ParseObject> query = ParseQuery.getQuery(KEY_PERSON);
-            query.getInBackground(objectId, (parseObject, e) -> {
-                if (e == null) {
-                    parseObject.deleteInBackground(e1 -> {
-                        if (e1 == null) {
-                            subscriber.onNext(null);
-                            subscriber.onCompleted();
+        return Observable.defer(() ->
+                Observable.create(subscriber -> {
+                    if (objectId == null) {
+                        // this should never happen in the app
+                        subscriber.onError(new Exception("incorrect usage"));
+                    }
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery(KEY_PERSON);
+                    query.getInBackground(objectId, (parseObject, e) -> {
+                        if (e == null) {
+                            parseObject.deleteInBackground(e1 -> {
+                                if (e1 == null) {
+                                    subscriber.onNext(null);
+                                    subscriber.onCompleted();
+                                } else {
+                                    subscriber.onError(e1);
+                                }
+                            });
                         } else {
-                            subscriber.onError(e1);
+                            subscriber.onError(e);
                         }
                     });
-                } else {
-                    subscriber.onError(e);
-                }
-            });
-        });
+                }));
     }
 }
