024df97383c1de14f3a4eee158ad6e8a4242d78c
Freeman Man <freeman@ycfreeman.com>
2015-08-16 16:46:47 +1000

    install parse idk

--- DementiaSmartWatch/caretaker/src/androidTest/java/com/justzed/caretaker/MainActivityTest.java
+++ DementiaSmartWatch/caretaker/src/androidTest/java/com/justzed/caretaker/MainActivityTest.java
@@ -1,23 +1,37 @@
 package com.justzed.caretaker;
 
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by freeman on 8/16/15.
  */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class MainActivityTest extends ActivityTestCase {
     public MainActivityTest() {
         super();
         setActivity(new MainActivity());
     }
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         super.setUp();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    @Test
+    public void someTest() {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
         super.tearDown();
     }
 }
