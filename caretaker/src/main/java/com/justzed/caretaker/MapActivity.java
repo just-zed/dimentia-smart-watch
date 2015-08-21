3f80ec67a9e7cfaf4389f03516f9df6e72ee96f5
Tristan Dubois <arcot92@gmail.com>
2015-08-21 21:49:36 +1000

    -Marker now moves from one point to another every few seconds (based on if the app is in the foreground or background). -The marker uses some linear interpolation to move from one point to another.

--- caretaker/src/main/java/com/justzed/caretaker/MapActivity.java
+++ caretaker/src/main/java/com/justzed/caretaker/MapActivity.java
@@ -1,35 +1,28 @@
 package com.justzed.caretaker;
 
+import android.graphics.Point;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.InterruptedIOException;
-import java.util.Random;
-
 import static com.google.android.gms.maps.CameraUpdateFactory.newLatLng;
-import static com.google.android.gms.maps.CameraUpdateFactory.newLatLngBounds;
+
 
 
 public class MapActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker patientMarker;
-    final Handler mapHandler = new Handler();
-    private boolean randomValue = true;
-
     private final double[] BRISBANE_LAT_LONG = new double[]{-27.471010,153.0333};
-    private final double[] NOT_BRISBANE_LAT_LONG = new double[]{-28.4667,154.0333};
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,51 +30,48 @@ public class MapActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_map);
         setUpMapIfNeeded();
+        countdownToNextUpdate(5000);
 
-        runThread();
-    }
 
-    private void runThread(){
-        new Thread(){
-            public void run(){
-                    try {
-                        runOnUiThread(new Runnable(){
-                            @Override
-                            public void run(){
-                                tempTestUpdate();
-                            }
-                        });
-                        Thread.sleep((3000));
-                    }
-                    catch(InterruptedException e){
-                        e.printStackTrace();
-
-                    }
-                }
-        }.start();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+        countdownToNextUpdate(5000);
     }
 
-    private void tempTestUpdate() {
+    @Override
+    protected void onPause(){
+        super.onPause();
+        countdownToNextUpdate(30000);
 
-        if(randomValue) {
-            updatePatientLocationOnMap(BRISBANE_LAT_LONG[0] , BRISBANE_LAT_LONG[1]);
-            randomValue = false;
-        }
-        else{
-            updatePatientLocationOnMap(NOT_BRISBANE_LAT_LONG[0] , NOT_BRISBANE_LAT_LONG[1]);
-            randomValue = true;
-        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+    }
+
+
+    public void countdownToNextUpdate(int timeBetweenUpdates){
+        new CountDownTimer(timeBetweenUpdates, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                LatLng coordinates = new LatLng(0,0);
+                updatePatientLocationOnMap(patientMarker,coordinates,false );
+            }
+        }.start();
     }
 
     /**
-     * Created by Tristan Dubois
-     *
      * This checks if the map needs to be set up.
      */
     private void setUpMapIfNeeded() {
@@ -123,10 +113,45 @@ public class MapActivity extends FragmentActivity {
      *
      * This method updates the patient marker to a new location
      */
-    public void updatePatientLocationOnMap(double patientCurrentLocationLong, double patientCurrentLocationLat){
-        patientMarker.setPosition(new LatLng(patientCurrentLocationLat, patientCurrentLocationLong));
+    public void updatePatientLocationOnMap(/*double patientCurrentLocationLong, double patientCurrentLocationLat,*/final Marker marker, final LatLng toPosition,
+                                           final boolean hideMarker){
+       // patientMarker.setPosition(new LatLng(patientCurrentLocationLat, patientCurrentLocationLong));
+
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = mMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 30000;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t)
+                        * startLatLng.latitude;
+                marker.setPosition(new LatLng(lat, lng));
+                mMap.moveCamera(newLatLng(patientMarker.getPosition()));
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+                    if (hideMarker) {
+                        marker.setVisible(false);
+                    } else {
+                        marker.setVisible(true);
+                    }
+                }
+            }
+        });
+
     }
-
-
 }
 
