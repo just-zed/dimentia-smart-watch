d4c591e0b54fadf6e782b70cae322806bbf88877
Freeman Man <freeman@ycfreeman.com>
2015-08-23 16:44:37 +1000

    WIP patient background service

--- patient/src/main/java/com/justzed/patient/MainActivity.java
+++ patient/src/main/java/com/justzed/patient/MainActivity.java
@@ -2,11 +2,22 @@ package com.justzed.patient;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.gson.Gson;
+import com.justzed.common.model.Person;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 
 public class MainActivity extends Activity {
+
+    private final Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,45 @@ public class MainActivity extends Activity {
         if (actionBar != null)
             actionBar.hide();
 
+
+        //first run check if patient is already created. if not create it
+        SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
+
+        if (!mPrefs.contains(PatientService.PREF_PATIENT_KEY)) {
+            //create patient and save
+
+            new Person(Person.PATIENT, generateToken())
+                    .save()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(person -> {
+                        // save person in app
+                        Editor editor = mPrefs.edit();
+                        String personJson = gson.toJson(person);
+                        editor.putString(PatientService.PREF_PATIENT_KEY, personJson);
+                        editor.apply();
+
+                        //start service
+                        Intent serviceIntent = new Intent(this, PatientService.class);
+                        serviceIntent.putExtra(PatientService.INTENT_PATIENT_KEY, personJson);
+                        startService(serviceIntent);
+                    });
+
+
+        } else {
+            //get patient from cache and start service
+            String personJson = mPrefs.getString(PatientService.PREF_PATIENT_KEY, "");
+            //start service
+            Intent serviceIntent = new Intent(this, PatientService.class);
+            serviceIntent.putExtra(PatientService.INTENT_PATIENT_KEY, personJson);
+            startService(serviceIntent);
+        }
+
+
+    }
+
+    private String generateToken() {
+        return "patient";
     }
 
 }
