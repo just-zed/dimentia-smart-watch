fd7edb862444a8aba91b758f5622159cf766348e
n8683069 <mitsuke1116@gmail.com>
2015-08-30 00:28:53 +1000

    Added new dependencies to "build.gradle" (https://parse.com/apps/quickstart#parse_push/android/native/existing), modified AndroidManifest.xml so that PanicButtonActivity will be activated, changed PanicButtonActivity so that methods of NotificationMessage class will be used after a button is pushed (but actually won't because of unsolved errors)

--- patient/src/main/java/com/justzed/patient/PanicButtonActivity.java
+++ patient/src/main/java/com/justzed/patient/PanicButtonActivity.java
@@ -1,36 +1,36 @@
 package com.justzed.patient;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.justzed.common.ApiKeys;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
 import com.parse.ParsePush;
 import com.parse.SaveCallback;
 
 public class PanicButtonActivity extends Activity {
+    Button button;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getActionBar();
+        setContentView(R.layout.activity_panic_button);
 
         //Add Parse API keys
         Parse.initialize(this, ApiKeys.PARSE_API_TEST_APPLICATION_ID, ApiKeys.PARSE_API_TEST_CLIENT_KEY);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
         //Enable Push notifications
         ParsePush.subscribeInBackground("", new SaveCallback() {
@@ -44,20 +44,40 @@ public class PanicButtonActivity extends Activity {
             }
         });
 
-        if (actionBar != null)
-            actionBar.hide();
-
-        Button button = (Button) findViewById(R.id.dummy_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener( new OnClickListener() {
+            public void onClick(View view) {
+                textView.setText("You clicked the button!");
                 NotificationMessage nm = new NotificationMessage();
-                nm.saveInstallation();
+
                 nm.setMessage("Test message from Hiroki");
                 nm.sendMessage();
-            }
-        });
 
+            }
+        }
+        );
+        textView = (TextView) findViewById(R.id.textView1);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_panic_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
