a2b7a6a16678a894945bc58b6b83e88910e11866
cuongtnn79 <cuongtnn.79@gmail.com>
2015-08-30 16:24:48 +1000

    CaretakerNfcTestCases is done.
    Adjust some methods in NfcActivity.java in Caretaker

--- caretaker/src/main/java/com/justzed/caretaker/NfcActivity.java
+++ caretaker/src/main/java/com/justzed/caretaker/NfcActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
@@ -44,23 +45,15 @@ public class NfcActivity extends Activity {
         setContentView(R.layout.activity_caretaker_nfc);
         text = (TextView) findViewById(R.id.text_view);
 
+        try {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+        catch (Exception e){
+            toast("Something is wrong !!!");
+        }
 
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        if ((checkNFCHardware()) && (checkNFCEnabled())) {
-
-            // Handle all of our received NFC intents in this activity.
-            mNfcPendingIntent = PendingIntent.getActivity(this, 0,
-                    new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-            // Intent filters for reading a note from a tag or exchanging over p2p.
-            IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-            try {
-                ndefDetected.addDataType("text/plain");
-            } catch (IntentFilter.MalformedMimeTypeException e) {
-            }
-            mNdefExchangeFilters = new IntentFilter[]{ndefDetected};
-
+        if ((checkNFCHardware()) && (checkNFCEnabled(mNfcAdapter))) {
+            receiveNdefMessage();
         }
 
         if (!mPrefs.contains(MainActivity.PREF_PERSON_KEY)) {
@@ -70,12 +63,40 @@ public class NfcActivity extends Activity {
         } else {
             caretakerToken = mPrefs.getString(MainActivity.PREF_PERSON_KEY, "");
         }
+    }
 
+    public void receiveNdefMessage(){
+        try {
+            // Handle all of our received NFC intents in this activity.
+            mNfcPendingIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
+            // Intent filters for reading a note from a tag or exchanging over p2p.
+            IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+            try {
+                ndefDetected.addDataType("text/plain");
+            } catch (IntentFilter.MalformedMimeTypeException e) {
+                toast("Something is wrong !!!");
+            }
+            mNdefExchangeFilters = new IntentFilter[]{ndefDetected};
+        }
+        catch (Exception e){}
     }
 
     private void enableNdefExchangeMode() {
         mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mNdefExchangeFilters, null);
+    }
+
+    private void disableNdefExchangeMode() {
+        if (NfcAdapter.getDefaultAdapter(this) != null){
+            mNfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableNdefExchangeMode();
     }
 
     @Override
@@ -148,48 +169,67 @@ public class NfcActivity extends Activity {
         return msgs;
     }
 
-
     // Check whether NFC hardware is available on device
-    private boolean checkNFCHardware() {
-        PackageManager pm = this.getPackageManager();
-
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
-            // NFC is not available on the device.
-            toast("The device does not has NFC hardware.");
-            return false;
-        } else {
-            // NFC and Android Beam file transfer is supported.
-            toast("NFC and Android Beam are supported on your device.");
+    public boolean checkNFCHardware() {
+        try {
+            PackageManager pm = this.getPackageManager();
+            if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+                // NFC is not available on the device.
+                toast("The device does not have NFC hardware.");
+                return false;
+            }
+            else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                // Android Beam feature is not supported.
+                toast("The device does not have Android Beam.");
+                return false;
+            }
+            else {
+                // NFC and Android Beam file transfer is supported.
+                toast("NFC and Android Beam are supported on your device.");
+                return true;
+            }
         }
-        return true;
+        catch (Exception e) {
+            toast("Something is wrong !!!");
+            return false;
+        }
     }
 
     // Check whether NFC is enabled on device
-    private boolean checkNFCEnabled() {
-        if (!mNfcAdapter.isEnabled()) {
-            toast("Please enable NFC.");
+    public boolean checkNFCEnabled(NfcAdapter mNfcAdapter) {
+        try {
+            if (!mNfcAdapter.isEnabled()) {
+                toast("Please enable NFC.");
 
-            // NFC is disabled, show the settings UI to enable NFC
-            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+                // NFC is disabled, show the settings UI to enable NFC
+                startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+                return false;
+            }
+            // Check whether Android Beam feature is enabled on device
+            else if (!mNfcAdapter.isNdefPushEnabled()) {
+                toast("Please enable Android Beam.");
+
+                // Android Beam is disabled, show the settings UI to enable Android Beam
+                startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
+                return false;
+            } else {
+                // NFC and Android Beam are enabled on device
+                toast("NFC and Android Beam are supported on your device.");
+                return true;
+            }
+        }
+        catch (Exception e){
+            toast("Something is wrong !!!");
             return false;
         }
-        // Check whether Android Beam feature is enabled on device
-        else if (!mNfcAdapter.isNdefPushEnabled()) {
-            toast("Please enable Android Beam.");
-
-            // Android Beam is disabled, show the settings UI to enable Android Beam
-            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
-            return false;
-        } else {
-            // NFC and Android Beam are enabled on device
-            toast("NFC and Android Beam are supported on your device.");
-        }
-        return true;
     }
 
     // Toast method
     private void toast(String text) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        try{
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception toast){ }
     }
 
 
