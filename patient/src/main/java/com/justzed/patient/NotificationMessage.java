6daa95f6a573093d34ecc0c906daa4e49232b5fc
Freeman Man <freeman@ycfreeman.com>
2015-09-06 17:40:34 +1000

    panic button is panicking!

--- patient/src/main/java/com/justzed/patient/NotificationMessage.java
+++ patient/src/main/java/com/justzed/patient/NotificationMessage.java
@@ -1,21 +1,18 @@
 package com.justzed.patient;
 
-import com.parse.ParseInstallation;
 import com.parse.ParsePush;
-import com.parse.ParseQuery;
-
-import java.util.LinkedList;
 
 /**
  * Created by takaha on 23/08/2015.
  */
 public class NotificationMessage {
-    private final ParsePush push = new ParsePush();
 
-    public void sendMessage(String message) {
+    public static void sendMessage(String channel, String message) {
+        ParsePush push = new ParsePush();
+        push.setChannel(channel);
         push.setMessage(message);
         push.sendInBackground();
-        
+
     }
 
 }
