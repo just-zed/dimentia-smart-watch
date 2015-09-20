a7b564034b21e3c8300e4ddfd80dfb423f44c0c1
Freeman Man <freeman@ycfreeman.com>
2015-09-20 15:24:59 +1000

    documentations

--- caretaker/src/main/java/com/justzed/caretaker/DialogActivity.java
+++ caretaker/src/main/java/com/justzed/caretaker/DialogActivity.java
@@ -7,6 +7,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 
+/**
+ * This is a "Dialog" activity that can be called using startActivity() and this displays an transparent
+ * activity and show a alertDialog in middle with
+ * <p>
+ * a positive button to launch CareTaker app and
+ * a negative button to simply close the dialog and this activity
+ *
+ * @author Freeman Man
+ * @since 2015-9-20
+ */
 public class DialogActivity extends Activity {
 
     public static String INTENT_KEY_MESSAGE = "DialogMessage";
@@ -35,6 +45,8 @@ public class DialogActivity extends Activity {
                     .setPositiveButton(R.string.launch_caretaker,
                             (dialog, which) -> {
                                 // launch main activity
+                                // TODO: fix app workflow so we can launch MapActivity instead.
+                                // currently we cannot do that as we requires MainActivity to get the patient
                                 Intent intent = new Intent(this, MainActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                                 startActivity(intent);
