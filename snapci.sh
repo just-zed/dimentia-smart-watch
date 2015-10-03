dc3d7330ad1a71ca432ae70e50d1601e28a7819b
Freeman Man <freeman@ycfreeman.com>
2015-10-03 20:44:34 +1000

    skip download-android if android-home exists

--- snapci.sh
+++ snapci.sh
@@ -7,9 +7,12 @@ set -e
 INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --format=%h -- $0)"
 ADB_INSTALL_TIMEOUT=100
 
-if [ ! -e ${INITIALIZATION_FILE} ]; then
-  # fetch and initialize $ANDROID_HOME
+if [ ! -e ${ANDROID_HOME} ]; then
+# fetch and initialize $ANDROID_HOME
   download-android
+fi
+
+if [ ! -e ${INITIALIZATION_FILE} ]; then
   # Use the latest android sdk tools
   echo y | android update sdk --no-ui --filter platform-tool > /dev/null
   echo y | android update sdk --no-ui --filter tool > /dev/null
