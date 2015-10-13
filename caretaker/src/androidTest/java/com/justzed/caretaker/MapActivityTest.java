d15801ba84e416a40bb07935b3a3ecc1289054a7
Freeman Man <freeman@ycfreeman.com>
2015-10-14 03:12:57 +1000

    more tries on travis

--- caretaker/src/androidTest/java/com/justzed/caretaker/MapActivityTest.java
+++ caretaker/src/androidTest/java/com/justzed/caretaker/MapActivityTest.java
@@ -1,33 +1,33 @@
-package com.justzed.caretaker;
-
-import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject;
-import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiSelector;
-import android.test.InstrumentationTestCase;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-
-/**
- * Created by Tristan on 14/08/2015.
- * <p>
- * This class is used to test the MapActivity Class.
- *
- * @author Tristan Dubois
- * @since 2015-08-14
- * <p>
- * TODO: map tests are not working, we'll fix it when we have time.
- */
+//package com.justzed.caretaker;
+//
+//import android.support.test.runner.AndroidJUnit4;
+//import android.support.test.uiautomator.UiDevice;
+//import android.support.test.uiautomator.UiObject;
+//import android.support.test.uiautomator.UiObjectNotFoundException;
+//import android.support.test.uiautomator.UiSelector;
+//import android.test.InstrumentationTestCase;
+//import android.test.suitebuilder.annotation.LargeTest;
+//
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.Marker;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//
+///**
+// * Created by Tristan on 14/08/2015.
+// * <p>
+// * This class is used to test the MapActivity Class.
+// *
+// * @author Tristan Dubois
+// * @since 2015-08-14
+// * <p>
+// * TODO: map tests are not working, we'll fix it when we have time.
+// */
 //@RunWith(AndroidJUnit4.class)
 //@LargeTest
 //public class MapActivityTest extends InstrumentationTestCase {
